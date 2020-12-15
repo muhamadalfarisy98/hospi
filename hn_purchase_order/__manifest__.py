349d79d2713aeb17d08788f87304c24c680bc869
Amirul <muhammad.amirul.muk@gmail.com>
2020-12-15 17:47:16 +0700

    init addons custom po

---
+++ hn_purchase_order/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Hospi PO',
+    'version': '14.0.1.0.0',
+    'author': 'Amirul',
+    'summary': 'Custom PO',
+    'website': '',
+    'description': """
+    """,
+    'data': [
+
+    ],
+    'depends': ['purchase_order'],
+    'auto_install': False,
+    'installable': True,
+    'application': False,
+}
