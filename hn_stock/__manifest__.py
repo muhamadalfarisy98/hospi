4658e7379220e1eec68bfe9004cdf2825f8c4527
Amirul <muhammad.amirul.muk@gmail.com>
2021-01-12 01:21:07 +0700

    add new addons: hn_stock. hide cost product outside accounting and purchase group

---
+++ hn_stock/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Hospi Stock',
+    'version': '14.0.1.0.0',
+    'author': 'Amirul',
+    'summary': 'Custom Stock, Product',
+    'website': '',
+    'description': """
+    """,
+    'data': [
+        'views/product_product_view.xml'
+    ],
+    'depends': ['stock', 'product'],
+    'auto_install': False,
+    'installable': True,
+    'application': False,
+}
