dec065fc93bda61dabb406fe20edd4a59ee036c3
Amirul <muhammad.amirul.muk@gmail.com>
2021-01-16 10:25:38 +0700

    change state code to state name on report layout ID

---
+++ hn_base/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Hospi Base',
+    'version': '14.0.1.0.0',
+    'author': 'Amirul',
+    'summary': 'Custom Base',
+    'website': '',
+    'description': """
+    """,
+    'data': [
+        'data/res_country_data.xml'
+    ],
+    'depends': ['base'],
+    'auto_install': False,
+    'installable': True,
+    'application': False,
+}
