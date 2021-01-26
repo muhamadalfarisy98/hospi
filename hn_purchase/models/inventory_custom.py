d6dc90b8d26cc6ae9e7640541fad607000722b1e
muhamadalfarisy98 <muhamadalfarisy9d@yahoo.com>
2021-01-26 20:57:04 +0700

    [ADD] computed field total koli, & koli/line

--- hn_purchase/models/inventory_custom.py
+++ hn_purchase/models/inventory_custom.py
@@ -4,16 +4,21 @@ from odoo import api, fields, models
 class KoliCustom(models.Model):
     _inherit = 'stock.picking'
 
-    total_koli = fields.Float(string='Total Koli')
-    # total_koli = fields.Float(string='Total Koli',compute='_get_total_koli')
-    koli = fields.Float(string='Koli')
+    # total_koli = fields.Float(string='Total Koli')
+    total_koli = fields.Float(string='Total Koli',compute='_get_total_koli')
+    
     
 
-    # @api.depends('move_line_ids_without','move_line_ids_without.qty_done')
-    # def _get_total_koli(self):
-    #     total=0
-    #     for r in self:
-    #         for line in r.move_line_ids_without:
-    #             total+=line.qty_done
-    #         r['total_koli']=total
+    @api.depends('move_line_ids_without_package','move_line_ids_without_package.koli')
+    def _get_total_koli(self):
+        total=0.0
+        for r in self:
+            for line in r.move_line_ids_without_package:
+                total+=line.koli
+            r['total_koli']=total
+
+class KoliEachCustom(models.Model):
+    _inherit='stock.move.line'
+    koli = fields.Float(string='Koli')
+
     
